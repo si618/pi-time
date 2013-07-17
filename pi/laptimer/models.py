4855bb5adc55587a7d6f7e3592a44412e3113f5a
si618 <sshnug.si+github@gmail.com>
2013-07-17 22:24:59 +0930

    Minor refactor.

--- pi/laptimer/models.py
+++ pi/laptimer/models.py
@@ -6,11 +6,11 @@ from django.utils.timezone import localtime
 
 class Track(models.Model):
 	name = models.CharField(max_length=64, unique=True)
-	distance = models.IntegerField(default=50)
+	distance = models.FloatField(default=50.0)
 	timeout = models.IntegerField(default=100)
 
 	def __unicode__(self):
-		return self.name    
+		return self.name
 
 class Session(models.Model):
 	name = models.CharField(max_length=64, unique=True)
@@ -37,27 +37,20 @@ class Lap(models.Model):
 		if (self.finish is None):
 			return 'Lap started ' + localtime(self.start).isoformat(' ')
 		else:
-			delta = self.finish - self.start
-			minutes = delta.seconds // 60
-			hours = delta.seconds // 3600
-			minutes -= hours * 60
-			seconds = delta.seconds - minutes * 60 - hours * 3600
-			milliseconds = delta.microseconds // 1000			
-			return 'Lap time ' + '%s:%s:%s.%s' % (hours, minutes, seconds, milliseconds)
+			return 'Lap time ' + self.timedelta_to_str(self.finish - self.start)
+
+	def timedelta_to_str(self, delta):
+		hours = delta.seconds // 3600
+		minutes = (delta.seconds // 60) - (hours * 60)
+		seconds = delta.seconds - minutes * 60 - hours * 3600
+		milliseconds = delta.microseconds // 1000
+		return '%s:%s:%s.%s' % (hours, minutes, seconds, milliseconds)
 
 class Settings(django_settings.db.Model):
 	unit_of_measurement = models.CharField(max_length=2, choices=settings.UNIT_OF_MEASUREMENT, default=settings.METRIC)
-	
+
 	class Meta:
 		abstract = True
 		verbose_name_plural = 'Settings'
 
 django_settings.register(Settings)
-
-'''
-class QueryCriteria(models.Model):
-	track = models.ForeignKey(Track)
-	session = models.ForeignKey(Session)
-	rider = models.ForeignKey(Rider)
-	top = IntegerField()
-'''
