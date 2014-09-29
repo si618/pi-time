8482c5601aba0e4c8c6eec7721ab8c5f4ddaf91d
si618 <sshnug.si+github@gmail.com>
2014-09-29 22:22:05 +0930

    Allow empty lines.

--- requirements.py
+++ requirements.py
@@ -3,7 +3,8 @@ from subprocess import call
 print 'Upgrading PYPI...'
 packages = [package[0:package.index('=')] for package in open('requirements.txt')]
 for package in packages:
-    call('pip install --upgrade --quiet ' + package, shell=True)
+    if len(package.strip()) > 0:
+        call('pip install --upgrade --quiet ' + package, shell=True)
 
 print '\nUpdating NPM...'
 call('npm update', shell=True)
